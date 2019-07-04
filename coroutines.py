28a43fe66703b780edb9b1a276d1b395e76210cf
root <root@localhost.localdomain>
2019-07-04 15:53:43 +0300

    First Coroutine

--- coroutines.py
+++ coroutines.py
@@ -1,1 +1,15 @@
-print ("Hello World")
+#Program to demonstrate coroutine execution
+def print_name(prefix):
+    print("Searching prefix:{}".format(prefix))
+    while True:
+        name = (yield)
+        if prefix in name:
+            print(name)
+
+#Calling coroutine, nothing will happen
+corou = print_name("Dear")
+
+corou.__next__()
+
+corou.send("Atul")
+corou.send("Dear Atul")
